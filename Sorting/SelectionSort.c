0f25dee76a6f67351283e9aa7467dc87729b86b8
sunko-seoul <sunko@student.42seoul.kr>
2023-03-17 22:59:53 +0900

    Sorting

---
+++ Sorting/SelectionSort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void SelSort(int arr[], int n)
+{
+    int i, j;
+    int maxIdx;
+    int tmp;
+
+    for (i = 0; i < n-1; i++)
+    {
+        maxIdx = i;
+        for (j = i+1; j < n; j++)
+        {
+            if (arr[j] < arr[maxIdx])
+                maxIdx = j;
+        }
+        tmp = arr[i];
+        arr[i] = arr[maxIdx];
+        arr[maxIdx] = tmp;
+    }
+}
+
+int main(void)
+{
+    int arr[4] = {3, 4, 2, 1};
+    int i;
+
+    SelSort(arr, sizeof(arr)/sizeof(int));
+    for(i = 0; i < 4; i++)
+        printf("%d ", arr[i]);
+    
+    printf("\n");
+    return (0);
+}
